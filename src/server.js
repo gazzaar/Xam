fa9ba8aecb324d7e79c29e2b0d2ffa88a02a1d3f
gazzaar <gazzarDev@gmail.com>
2025-03-14 06:17:05 +0200

    use urlencoded to pass req body

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const indexRouter = require('./routes/index.js');
+const studentsRouter = require('./routes/studentsRoutes.js');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/students', studentsRouter);
 
 app.listen(3000);
